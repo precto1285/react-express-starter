29a9d083d46d509eb3e697f0898f99412f23d89c
Philippe Recto <precto1285@gmal.com>
2019-05-15 14:25:55 -0700

    added npm init

--- react-express-msql/src/App.js
+++ react-express-msql/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Express MySQL</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sentence: "React Express MySQL",
+      show: false
+    }
+  }
+  showHandler() {
+    const showSentence = !this.state.show;
+    this.setState({
+      show: showSentence
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <button onClick={this.showHandler.bind(this)}>Show</button>
+
+        {
+          this.state.show ?
+            <div>
+              <h1>{this.state.sentence}</h1>
+            </div>
+            : null
+        }
+
+      </div>
+    );
+  }
 }
 
 export default App;
